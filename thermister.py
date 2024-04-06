70ed86417e70dd3bfa55c5c57911e07f975bb275
Luke Zimmerman <lukezimm2014@gmail.com>
2024-04-06 14:56:47 -0400

    adc test

--- thermister.py
+++ thermister.py
@@ -2,6 +2,7 @@ import os, math, time
 from gpiozero import InputDevice, OutputDevice
 import csv
 import adafruit_lps2x
+from cedargrove_nau7802 import NAU7802
 import board
 
 # thermistor script
@@ -25,23 +26,32 @@ thermistor = InputDevice(7) # gpio pin to read thermister
 i2c = board.I2C() # i2c connection to read accelerometer data from icm 20649
 #icm = adafruit_icm20x.ICM20649(i2c) # accelerometer object
 lps = adafruit_lps2x.LPS22(i2c) # barometer / altimeter object
+nau7802 = NAU7802(board.I2C(), address=0x2A, active_channels=2)
 
-
-# init variables
-#sample_data=[apogeeAltitude]*ROLLING_AVERAGE_SAMPLES # set all altitude samples to the apogee altitude initially
-#oldestDataIndex = 0
-
-# wait for boost
-# while(not IMU_read() or not quark_read()):
-#   time.sleep(sampleTimeDelay)
-
-# redundant check for boost
-# wait for apogee
-print('waiting for apogee signal from quark...')
-while(not apogee_detect.is_active): # wait until quark sends apogee signal
-  time.sleep(SAMPLE_INTERVAL)
-print('apogee reached.')
-# apoogee reached
+def zero_channel():
+    """Initiate internal calibration for current channel.Use when scale is started,
+    a new channel is selected, or to adjust for measurement drift. Remove weight
+    and tare from load cell before executing."""
+    print(
+        "channel %1d calibrate.INTERNAL: %5s"
+        % (nau7802.channel, nau7802.calibrate("INTERNAL"))
+    )
+    print(
+        "channel %1d calibrate.OFFSET:   %5s"
+        % (nau7802.channel, nau7802.calibrate("OFFSET"))
+    )
+    print("...channel %1d zeroed" % nau7802.channel)
+    
+def read_raw_value(samples=2):
+    """Read and average consecutive raw sample values. Return average raw value."""
+    sample_sum = 0
+    sample_count = samples
+    while sample_count > 0:
+        while not nau7802.available():
+            pass
+        sample_sum = sample_sum + nau7802.read()
+        sample_count -= 1
+    return int(sample_sum / samples)
 
 def altitude(pressure: float) -> float:
     """
@@ -49,32 +59,74 @@ def altitude(pressure: float) -> float:
     """
     return 145366.45 * (1 - (pressure / 1013.25)**(1/5.255))
 
-current_altitude = altitude(lps.pressure) # get altitude from lps sensor
-print(current_altitude)
-offset = current_altitude
-print(current_altitude - offset)
 
-# descent, but not dis-reefing yet
-# start warming up heater
-while(current_altitude > ALTITUDE_DISREEF): # while we are above the dis-reef altitude
-    # read thermister
-    voltage = thermistor.value # read voltage from thermistor
-    # TODO: convert voltage to temperature
-    temp = temp(voltage) # convert voltage to temp via lookup table
+def temp(voltage: float) -> float:
+    """
+    Convert voltage to temperature
+    """
+    
 
-    # check if we need to turn on or off the heater
-    # by trying to maintain a temperature range
-    if(temp < TEMP_LOWER_LIMIT):
-        heater.on() # turn on heater 
-    if(temp > TEMP_UPPER_LIMIT):
-        heater.off() # turn off heater
-    time.sleep(SAMPLE_INTERVAL)
-    # update altitude for next iteration
-    current_altitude = altitude(lps.pressure) - offset
+def main():
+    enabled = nau7802.enable(True)
+    print(f"NAU7802 enabled: {enabled}")
+    
+    nau7802.channel = 1
+    zero_channel()  # Calibrate and zero channel
+    nau7802.channel = 2
+    zero_channel()  # Calibrate and zero channel
+    
+    while True:
+        nau7802.channel = 1
+        value = read_raw_value()
+        print("channel %1.0f raw value: %7.0f" % (nau7802.channel, value))
+
+        nau7802.channel = 2
+        value = read_raw_value()
+        print("channel %1.0f raw value: %7.0f" % (nau7802.channel, value))
+
+    print("READY")
+    
+    # wait for boost
+    # while(not IMU_read() or not quark_read()):
+    #   time.sleep(sampleTimeDelay)
+
+    # redundant check for boost
+    # wait for apogee
+    print('waiting for apogee signal from quark...')
+    while(not apogee_detect.is_active): # wait until quark sends apogee signal
+        time.sleep(SAMPLE_INTERVAL)
+    print('apogee reached.')
+    # apoogee reached
+
+    current_altitude = altitude(lps.pressure) # get altitude from lps sensor
     print(current_altitude)
+    offset = current_altitude
+    print(current_altitude - offset)
+    
+    # descent, but not dis-reefing yet
+    # start warming up heater
+    while(current_altitude > ALTITUDE_DISREEF): # while we are above the dis-reef altitude
+        # read thermistor
+        voltage = thermistor.value # read voltage from thermistor
+        # TODO: convert voltage to temperature
+        temperature = temp(voltage) # convert voltage to temp via lookup table
+        #temp = temp(voltage) # convert voltage to temp via lookup table
 
-# dis-reefing altitude reached
-heater.on() # turn on heater fully
-# wait for deployment
-time.sleep(20)
-heater.off() # turn off heater
+        # check if we need to turn on or off the heater
+        # by trying to maintain a temperature range
+        if(temp < TEMP_LOWER_LIMIT):
+            heater.on() # turn on heater 
+        if(temp > TEMP_UPPER_LIMIT):
+            heater.off() # turn off heater
+        time.sleep(SAMPLE_INTERVAL)
+        # update altitude for next iteration
+        current_altitude = altitude(lps.pressure) - offset
+        print(current_altitude)
+
+    print('heater fully on')
+
+    # dis-reefing altitude reached
+    heater.on() # turn on heater fully
+    # wait for deployment
+    time.sleep(20)
+    heater.off() # turn off heater
