1af3d965a5b56573314584375cf084349e924498
Luke Zimmerman <lukezimm2014@gmail.com>
2024-04-19 00:08:13 -0400

    apogee detect

--- thermister.py
+++ thermister.py
@@ -2,6 +2,7 @@ import os, math, time
 from gpiozero import InputDevice,OutputDevice
 import csv
 import adafruit_lps2x
+#3import adafruit_icm20x
 from cedargrove_nau7802 import NAU7802, LDOVoltage
 import board
 
@@ -10,25 +11,20 @@ import board
 # implement lookup table
 
 # init constants
-# TODO: some still need to be changed based on testing
-SAMPLE_INTERVAL: float = .5 # how often data is sampled (seconds)
-ROLLING_AVERAGE_DURATION: float = 10 # number of seconds of data to average
+SAMPLE_INTERVAL: float = .5 # how often data is sampled (seconds) # TODO:
+ROLLING_AVERAGE_DURATION: float = 10 # number of seconds of data to average # TODO: 
 ROLLING_AVERAGE_SAMPLES: int = math.ceil(ROLLING_AVERAGE_DURATION / SAMPLE_INTERVAL) # number of samples to average
 # heater constants
 TEMP_UPPER_LIMIT: float = 150 # temp at which the heater should be turned off (celsius)
 TEMP_LOWER_LIMIT: float = 130 # temp at which the heater should be turned on (celsius)
-ALTITUDE_DISREEF: float = 1500 # altitude to start melting the wire (ft)
-# thermistor constants
-THERMISTOR_NOMINAL: float = 10000 # resistance at 25 degrees C   
-TEMPERATURE_NOMINAL: float = 25 # temp. for nominal resistance (almost always 25 C)
-NUM_SAMPLES: int = 5 # num of samples to average
-B_COEFFICIENT: float = 3950; # The beta coefficient of the thermistor (usually 3000-4000)
-SERIES_RESISTOR: float = 10000 # The value of the 'other' resistor
+R_UPPER_LIMIT: float = 1770 # resistance at which the heater should be turned off (ohms)
+R_LOWER_LIMIT: float = 2970 # resistance at which the heater should be turned on 
+ALTITUDE_DISREEF: float = 1500 # altitude to start melting the wire (ft) # TODO:
+
 
 # init gpio, i2c
-apogee_detect = InputDevice(4) # gpio pin to detect apogee from quark
-heater = OutputDevice(6) # gpio pin to activate heater
-# TODO: choose correct gpio pin for thermister and correct inputdevice
+apogee_detect = InputDevice(15) # gpio pin to detect apogee from quark # TODO:
+heater = OutputDevice(6) # gpio pin to activate heater # TODO: 
 i2c = board.I2C() # i2c connection to read accelerometer data from icm 20649
 #icm = adafruit_icm20x.ICM20649(i2c) # accelerometer object
 lps = adafruit_lps2x.LPS22(i2c) # barometer / altimeter object
@@ -47,12 +43,24 @@ def altitude(pressure: float) -> float:
 def temp(voltage: float) -> float:
     R = -4700*voltage/(voltage-3)
     print(R)
-    # 130C = 2.94V
-    # 150C = 1.77V
+    # 130C = 2.94 R
+    # 150C = 1.77 R
+    
+    # 140C = 2.27 R 
+    # 
     
     """
     Convert voltage to temperature
     """
+    return R
+    
+def imu_read() -> bool:
+    """
+    Returns true if the acceleration magnitude is greater than 5
+    """
+    icm.acceleration # read acceleration data
+    return math.sqrt(icm.acceleration[0]**2 + icm.acceleration[1]**2 + icm.acceleration[2]**2) > 5
+    
     
 def zero_channel():
     print("channel %d zero offset: %7.0f" % (nau7802.channel, nau7802.calibrate("INTERNAL")))
@@ -69,70 +77,67 @@ def read_raw_value(samples=2):
         sample_count -= 1
     return int(sample_sum / samples)
 
+
+
 def main():
     time.sleep(3)
     nau7802.channel=1
     zero_channel()
-    
-
-    while True:
-        nau7802.channel=1
-        value=read_raw_value()
-        V=float("%7.0f"%value)
-        print(V)
-        T=temp(V)
-        print(T)
 
     print("READY")
     
     # wait for boost
-    # while(not IMU_read() or not quark_read()):
-    #   time.sleep(sampleTimeDelay)
-
-main()
-
-# redundant check for boost
-# wait for apogee
-
-#while(not apogee_detect.is_active): # wait until quark sends apogee signal
-#    time.sleep(SAMPLE_INTERVAL)
-
-# apoogee reached
-current_altitude = altitude(lps.pressure) # get altitude from lps sensor
-print(current_altitude)
-offset = current_altitude
-print(current_altitude - offset)
-
-# descent, but not dis-reefing yet
-# start warming up heater
-while(current_altitude > ALTITUDE_DISREEF): # while we are above the dis-reef altitude
-    # read thermistor
-    voltage = thermistor.value # read voltage from thermistor
-    # TODO: convert voltage to temperature
-    print('voltage: ', voltage)
-    temperature = temp(voltage) # convert voltage to temp via lookup table
-    print('temperature: ', temperature)
-    #temp = temp(voltage) # convert voltage to temp via lookup table
-
-    # check if we need to turn on or off the heater
-    # by trying to maintain a temperature range
-    if(temperature < TEMP_LOWER_LIMIT):
-        heater.on() # turn on heater 
-        print('heater on')
-    if(temperature > TEMP_UPPER_LIMIT):
-        heater.off() # turn off heater
-        print('heater off')
-    time.sleep(SAMPLE_INTERVAL)
-    # update altitude for next iteration
-    current_altitude = altitude(lps.pressure) - offset
+    #while(not imu_read()): # or not quark_read()):
+    #    time.sleep(SAMPLE_INTERVAL)
+        
+    # boost detected
+    
+    # wait for apogee
+    current_altitude = altitude(lps.pressure) # get altitude from lps sensor
     print(current_altitude)
+    offset = current_altitude
+    print(current_altitude - offset)
 
-print('heater fully on')
+    while(not apogee_detect.is_active): # wait until quark sends apogee signal
+        time.sleep(SAMPLE_INTERVAL)
+    print("wooooooooooo")
 
-# dis-reefing altitude reached
-heater.on() # turn on heater fully
-# wait for deployment
-time.sleep(20)
-heater.off() # turn off heater
+    # apogee reached
+
+    # descent, but not dis-reefing yet
+    # start warming up heater
+    while(current_altitude > ALTITUDE_DISREEF): # while we are above the dis-reef altitude
+        # read thermistor
+        nau7802.channel=1
+        value=read_raw_value()
+        V=1.5*float("%7.0f"%value)/(16777215) # convert to voltage
+        print('voltage: ', V)
+        # TODO convert voltage to temperature
+        #temperature = temp(voltage) # convert voltage to temp via lookup table
+        R = temp(V) # convert voltage to resistance
+        #print('temperature: ', temperature)
+        #temp = temp(voltage) # convert voltage to temp via lookup table
+
+        # check if we need to turn on or off the heater
+        # by trying to maintain a temperature range
+        #if(temperature < TEMP_LOWER_LIMIT):
+        if(R > TEMP_LOWER_LIMIT):
+            heater.on() # turn on heater 
+            print('heater on')
+        #if(temperature > TEMP_UPPER_LIMIT):
+        if(R < TEMP_UPPER_LIMIT):
+            heater.off() # turn off heater
+            print('heater off')
+        time.sleep(SAMPLE_INTERVAL)
+        # update altitude for next iteration
+        current_altitude = altitude(lps.pressure) - offset
+        print(current_altitude)
+
+    # dis-reefing altitude reached
+    print('disreef')
+    heater.on() # turn on heater fully
+    # wait for deployment
+    time.sleep(20)
+    heater.off() # turn off heater
 
 main()
